86cf66023dbf3162fbe234f7d99d5f728ac22aac
carlosrdev <carlosrdev@gmail.com>
2019-03-26 21:26:44 -0600

    Exclusion de Licencias
    En el gradle a nivel Module: app excluimos las licencias de REST

---
+++ app/src/main/java/com/example/elementosgeolocalizacion/Localizacion.kt
@@ -0,0 +1,6 @@
+package com.example.elementosgeolocalizacion
+
+class Localizacion {
+    var lat:Double?=null
+    var lon:Double?=null
+}
---
+++ app/src/main/java/com/example/elementosgeolocalizacion/Usuario.kt
@@ -0,0 +1,7 @@
+package com.example.elementosgeolocalizacion
+
+class Usuario {
+    var id:Int?=null
+    var nombre:String?=null
+    var localizacion:Localizacion?=null
+}
